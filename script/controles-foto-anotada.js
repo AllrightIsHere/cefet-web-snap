03a5d78f97fa37ced8f8e678adf9eaf7e26e988d
AllrightIsHere <gasilva912@gmail.com>
2021-12-06 00:33:12 -0300

    Desafio 1: Trocar a foto por outro arquivo

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -1,7 +1,28 @@
+// exercício 04
+
 const filtroEl = document.querySelector('#filtro-da-foto');
 
 const imgEl = document.querySelector('.foto-anotada > img');
 
 filtroEl.addEventListener('change', (e) => {
     imgEl.style.filter = filtroEl.value;
+});
+
+// Desafio 01
+const inputFile = document.querySelector('#imagem');
+
+inputFile.addEventListener('change', (e) => {
+    const file = e.target.files && e.target.files.length ? e.target.files[0] : null;
+
+    if (file && file.type.startsWith('image/')) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', (e) => {
+            imgEl.src = e.target.result;
+        });
+
+        reader.readAsDataURL(file);
+    }
+
+
 });
